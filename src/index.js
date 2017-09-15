3c90f4fd75623262e129d90df94f0558f3c9568f
Han <allen0346@gmail.com>
2017-09-15 10:00:15 +0800

    [Feature] bookmark and version 2.2.0

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import indexPage from './reducers/index-page'
 import { denormalizePosts, denormalizeTopics } from './utils/denormalize-asset'
 import { post, posts } from './reducers/posts'
 import { topic, topics } from './reducers/topics'
+import bookmarks from './reducers/bookmarks'
 import { fetchIndexPageContent, fetchCategoriesPostsOnIndexPage } from './actions/index-page'
 import { fetchAFullPost, fetchListedPosts, fetchEditorPickedPosts,
   fetchInfographicPostsOnIndexPage, fetchPhotographyPostsOnIndexPage } from './actions/posts'
 import { fetchAFullTopic, fetchTopics, fetchTopicsOnIndexPage } from './actions/topics'
+import { createBookmark, getBookmarks, deleteBookmark, getCurrentBookmark } from './actions/bookmarks'
 
 export default {
   actions: {
@@ -23,6 +25,10 @@ export default {
     fetchTopicsOnIndexPage,
     fetchInfographicPostsOnIndexPage,
     fetchPhotographyPostsOnIndexPage,
+    createBookmark,
+    getBookmarks,
+    deleteBookmark,
+    getCurrentBookmark,
   },
   reducers: {
     entities,
@@ -31,6 +37,7 @@ export default {
     topic,
     topics,
     indexPage,
+    bookmarks,
   },
   reduxStateFields,
   utils: {
