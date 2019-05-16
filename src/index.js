4866f2018d9a9f857a516b477fd0e1e3dba7aeca
yucj <nakts0123@gmail.com>
2019-05-16 19:35:53 +0800

    export actions

--- src/index.js
+++ src/index.js
@@ -1,47 +1,21 @@
+import { denormalizePosts, denormalizeTopics } from './utils/denormalize-asset'
+import { post, posts } from './reducers/posts'
+import { topic, topics } from './reducers/topics'
+import actions from './actions'
+import createStore from './store/create-store'
 import entities from './reducers/entities'
 import formAPIURL from './utils/form-api-url'
-import createStore from './store/create-store'
 import indexPage from './reducers/index-page'
 import pagination from './utils/pagination'
 import reduxStateFields from './constants/redux-state-field-names'
 import ReduxStoreContext from './context/redux-store'
 import ReduxStoreProvider from './component/provider'
-import { denormalizePosts, denormalizeTopics } from './utils/denormalize-asset'
-import { post, posts } from './reducers/posts'
-import { topic, topics } from './reducers/topics'
-import {
-  fetchIndexPageContent,
-  fetchCategoriesPostsOnIndexPage,
-} from './actions/index-page'
-import {
-  fetchAFullPost,
-  fetchListedPosts,
-  fetchEditorPickedPosts,
-  fetchInfographicPostsOnIndexPage,
-  fetchPhotographyPostsOnIndexPage,
-} from './actions/posts'
-import {
-  fetchAFullTopic,
-  fetchTopics,
-  fetchTopicsOnIndexPage,
-} from './actions/topics'
 
 export default {
   createStore,
   ReduxStoreProvider,
   ReduxStoreContext,
-  actions: {
-    fetchEditorPickedPosts,
-    fetchCategoriesPostsOnIndexPage,
-    fetchTopics,
-    fetchAFullPost,
-    fetchAFullTopic,
-    fetchListedPosts,
-    fetchIndexPageContent,
-    fetchTopicsOnIndexPage,
-    fetchInfographicPostsOnIndexPage,
-    fetchPhotographyPostsOnIndexPage,
-  },
+  actions,
   reducers: {
     entities,
     post,
